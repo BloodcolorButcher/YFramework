5028cefd67bebf7bebe0e9189d0959a7cb000de0
yangyue <1079725446@qq.com>
2023-08-02 11:32:52 +0800

    添加服务器，服务器从项目里面移出来

---
+++ Unity/Server/Server/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("123");
+
+Console.WriteLine("Hello, World!");
+string str = Console.ReadLine()??"";
+Console.WriteLine(str);
+Console.ReadLine();
